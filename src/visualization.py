a637cf238e360d691c5a77aae81a535379bca3a4
roberta <roberta_huang@icloud.com>
2018-12-24 18:26:54 +0100

    added doc to visualization

--- src/visualization.py
+++ src/visualization.py
@@ -4,6 +4,14 @@ from sklearn.decomposition import PCA
 
 
 def plot_weights_mean(weights, title="Weights Mean over Generations", xlabel="Generations", ylabel="Weights Mean"):
+    """
+    Plot the mean for each generation's weights
+
+    :param weights: array of weights (ndarray) obtained during each generation
+    :param title: plot's title
+    :param xlabel: plot's x label
+    :param ylabel: plot's y label
+    """
     means = []
     for w in weights:
         means.append(np.mean(w))
@@ -15,8 +23,16 @@ def plot_weights_mean(weights, title="Weights Mean over Generations", xlabel="Ge
 
 
 def plot_weights_2d(weights, title="Weights Mean over Generations", xlabel="Generations", ylabel="Weights Mean"):
+    """
+    Plot the weights reduced in 2d
+
+    :param weights: array of weights (ndarray) obtained during each generation
+    :param title: plot's title
+    :param xlabel: plot's x label
+    :param ylabel: plot's y label
+    """
     pca = PCA(n_components=2)
-    weights_2d = pca.fit_transform(weights)
+    weights_2d = pca.fit_transform(weights)  # TODO fix weights type
     plt.figure(figsize=(20, 10))
     plt.plot(weights_2d)
     plt.title(title, fontsize=18)
@@ -25,10 +41,18 @@ def plot_weights_2d(weights, title="Weights Mean over Generations", xlabel="Gene
 
 
 def plot_weights_difference(weights, title="Weights Diffs over Generations", xlabel="Generations", ylabel="Weights"):
+    """
+    Plot the difference of centroids of weights wrt to the previous generation's weights
+
+    :param weights: array of weights (ndarray) obtained during each generation
+    :param title: plot's title
+    :param xlabel: plot's x label
+    :param ylabel: plot's y label
+    """
     diffs = []
     for i, w in enumerate(weights):
         if i != 0:
-            diffs.append(np.mean(w) - np.mean(weights[i-1]))
+            diffs.append(np.mean(w) - np.mean(weights[i - 1]))
     plt.figure(figsize=(20, 10))
     plt.plot(diffs)
     plt.title(title, fontsize=18)
@@ -37,6 +61,15 @@ def plot_weights_difference(weights, title="Weights Diffs over Generations", xla
 
 
 def plot_scores(scores, title="Scores over generations", xlabel="Generations", ylabel="Scores"):
+    """
+    Plot the mean, std bands and max of scores obtained during training
+
+    :param scores: scores (ndarray) obtained by each agent during each generation (e.g. if using results returned by
+                   run_agent_genetic(), scores = np.array(results[:, 1, :], dtype=float))
+    :param title: plot's title
+    :param xlabel: plot's x label
+    :param ylabel: plot's y label
+    """
     x_ticks = np.arange(len(scores))
     means = np.mean(scores, axis=1)
     stds = np.std(scores, axis=1)
@@ -49,5 +82,3 @@ def plot_scores(scores, title="Scores over generations", xlabel="Generations", y
     plt.xlabel(xlabel, fontsize=18)
     plt.ylabel(ylabel, fontsize=18)
     plt.legend(fontsize=16)
-
-
