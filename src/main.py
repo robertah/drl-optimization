81ebec65e486ed05e97355560fcee80cf9dfb96b
roberta <roberta_huang@icloud.com>
2019-01-04 12:44:59 +0100

    added termination condition for genetic + weights perturbation analysis

--- src/main.py
+++ src/main.py
@@ -1,15 +1,13 @@
 from genetic import run_agent_genetic, Agent
 from utils import get_results, get_best_agent
+from analysis import perturbate_weights
 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
-# agents_weights, scores, children = run_agent_genetic(n_agents=50, n_generations=50, save=True)
+# agents_weights, scores, children = run_agent_genetic(n_agents=50, n_generations=20, save=True)
 
-weights, scores = get_results()
 
-best_weights = get_best_agent()
-
-agent = Agent(weights=best_weights)
-agent.run_agent(render=True)
+weights = get_best_agent(mean=True, timestamp='20190103183514')
+perturbate_weights(weights)
