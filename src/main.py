ae2bd0e8cfeb0443ad6be30a703161467dd42f8b
roberta <roberta_huang@icloud.com>
2019-01-06 10:54:28 +0100

    reimplemented genetic algorithm

--- src/main.py
+++ src/main.py
@@ -1,13 +1,22 @@
-from genetic import run_agent_genetic, Agent
-from utils import get_results, get_best_agent
-from analysis import perturbate_weights
-
 import os
+
+from genetic import Population
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 # agents_weights, scores, children = run_agent_genetic(n_agents=50, n_generations=20, save=True)
 
 
-weights = get_best_agent(mean=True, timestamp='20190103183514')
-perturbate_weights(weights)
+# weights = get_best_agent(mean=True, timestamp='20190103183514')
+# perturbate_weights(weights)
+
+
+agents = Population(population_size=50,  # n agents
+                    max_generations=100,  # max n generations
+                    n_selected=0.3,  # n agents selected for crossover
+                    mutation_rate=0.3,  # probability of mutation
+                    mutation_noise=1,  # gaussian noise scale for mutation
+                    elite=2  # n best agents kept for next generation
+                    )
+agents.evolve()
