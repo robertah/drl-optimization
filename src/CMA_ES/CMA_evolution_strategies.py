9953acb64c5d681885852684e55029e9d5ba7653
roberta <roberta_huang@icloud.com>
2019-01-09 15:56:37 +0100

    implemented Covariance-Matrix Adaptation Evolution Strategy

---
+++ src/CMA_ES/CMA_evolution_strategies.py
@@ -0,0 +1,66 @@
+import numpy as np
+from scipy.stats import multivariate_normal
+
+from optimizers import EvolutionaryOptimizers
+
+
+class CMAEvolutionStrategies(EvolutionaryOptimizers):
+
+    def __init__(self, perc_selelcted):
+        self.perc_selected = perc_selelcted
+        super(CMAEvolutionStrategies, self).__init__()
+
+    def selection(self, scores, population_size):
+        """
+        Select the best performing agents in the previous generation as new parents
+
+        :param scores:
+        :type scores:
+        :param population_size:
+        :type population_size:
+        :return:
+        """
+        n_selected = int(self.perc_selected * population_size)
+        selected_parents = np.argsort(-scores)[:n_selected]
+        return selected_parents
+
+    @staticmethod
+    def compute_covariance(parents):
+        parents -= np.mean(parents, axis=0)
+        covariance = np.dot(parents.T, parents.conj()) / (len(parents) - 1)
+        return covariance
+
+    def sample_children(self, population, parents):
+        weights_size = len(population.agents_weights[0][0])
+        sampled = np.empty(weights_size, dtype=np.ndarray)
+        for j in range(weights_size):
+            temp = parents[:, j]
+            orig_shape = temp[0].shape
+            parents_flatten = np.empty((temp.shape[0], np.prod(temp[0].shape)), dtype=float)
+            for i, p in enumerate(temp):
+                parents_flatten[i] = p.ravel()
+
+            # scaler = StandardScaler()
+            # parents_scaled = scaler.fit_transform(parents_flatten)
+            mean = np.mean(parents_flatten, axis=0)
+            covariance = self.compute_covariance(parents_flatten)
+            children = np.random.multivariate_normal(mean, covariance, size=population.size)
+            if len(orig_shape) == 1:
+                reshape = (population.size, orig_shape[0])
+            else:
+                reshape = (population.size, orig_shape[0], orig_shape[1])
+            sampled[j] = children.reshape(reshape)
+
+        return sampled
+
+    def generate_next_generation(self, population, generation):
+        # get ids of the best performing agents
+        id = self.selection(scores=population.scores[generation], population_size=population.size)
+        sampled = self.sample_children(population, population.agents_weights[generation][id])
+        next_generation = generation + 1
+        for i in range(population.size):
+            child = []
+            for j in range(sampled.shape[0]):
+                child.append(sampled[j][i])
+            population.agents_weights[next_generation][i] = child
+
