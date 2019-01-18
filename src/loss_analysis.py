89617e5d90ae3ccd3015e86005529cfc8d322b61
roberta <roberta_huang@icloud.com>
2019-01-18 10:58:56 +0100

    merged perturbate weights in loss analysis

--- src/loss_analysis.py
+++ src/loss_analysis.py
@@ -7,6 +7,26 @@ from matplotlib import cm
 from scipy.spatial.distance import euclidean
 
 
+def perturbate_weights(weights, n=5, noise_scale=1, render=False):
+    """
+    Randomly perturbate final weights, to verify the optimality condition
+
+    :param weights: weights of the agent
+    :type weights: array of weights and bias of neural network's layers
+    :param n: number of perturbations
+    :type n: int
+    :param noise_scale: gaussian noise scale
+    :type noise_scale: int or float
+    :param render: animate the agent
+    :type render: bool
+    """
+    for i in range(n):
+        noise = np.random.normal(scale=noise_scale)
+        agent = Agent(weights=weights + noise)
+        score = agent.run_agent(render=render)
+        print("{} - score: {}".format(i, score))
+
+
 def interpolate(initial_agent, final_agent, objective, n_steps):
     """
     interpolation method to evaluate the path from initial weights to final weights
