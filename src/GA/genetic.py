afbee10a088c0e1315bdab438040ebc72f501caa
roberta <roberta_huang@icloud.com>
2019-01-19 12:35:26 +0100

    deleted unused genetic functions

--- src/GA/genetic.py
+++ src/GA/genetic.py
@@ -25,7 +25,6 @@ def run_agent_es(n_agents=50, n_generations=100, save=True):
 
     for i in range(n_generations):
         agents_weights[i] = np.array([a.model.get_weights() for a in agents], dtype=np.ndarray)
-        print(i)
 
         for j, agent in enumerate(agents):  # TODO parallelize
             scores[i][j] = agent.run_agent()
@@ -36,96 +35,5 @@ def run_agent_es(n_agents=50, n_generations=100, save=True):
 
     if save:
         save_results(agents_weights, scores)
-
-    return agents_weights, scores, children
-
-
-def run_agent_genetic_alternative(n_init_agents=300, n_agents=50, n_generations=100, save=True):
-    """
-    In this version we have a different number of agents in the inizialization in order to better expore the parameter
-    pace at the beginning.
-    This is useful just in the inizialization "to take the right direction in the parameter space" so it would be
-    useless and computationally expensive use so much agents once the alg is initialized.
-
-    :param n_init_agents:
-    :param n_agents:
-    :param n_generations:
-    :param save:
-    :return:
-    """
-    n_weights = len(Agent().model.get_weights())
-
-    agents_weights = np.empty((n_generations - 1, n_agents, n_weights), dtype=np.ndarray)
-    agents_init_weights = np.empty((n_init_agents, n_weights), dtype=np.ndarray)
-    scores = np.empty((n_generations - 1, n_agents), dtype=float)
-    init_scores = np.empty((n_init_agents), dtype=float)
-
-    children = np.empty((n_generations, n_weights), dtype=np.ndarray)
-
-    # initialize agents
-    init_agents = [Agent() for _ in range(n_init_agents)]
-    agents = [Agent() for _ in range(n_agents)]
-
-    for i in range(n_generations):
-        print(i)
-        if i == 0:
-            agents_init_weights = np.array([a.model.get_weights() for a in init_agents], dtype=np.ndarray)
-            for j, agent in enumerate(init_agents):  # TODO parallelize
-                init_scores[j] = agent.run_agent()
-            child = crossover_function(init_agents, init_scores)
-
-        else:
-            agents_weights[i - 1] = np.array([a.model.get_weights() for a in agents], dtype=np.ndarray)
-            for j, agent in enumerate(agents):  # TODO parallelize
-                scores[i - 1][j] = agent.run_agent()
-            child = crossover_function(agents, scores[i - 1])
-
-        children[i] = np.array(child, dtype=np.ndarray).reshape(n_weights)
-        agents = generate_population(child, n_agents, agents)
-
-    if save:
-        save_results(agents_weights, scores)
-
-    return agents_weights, scores, children
-
-
-def run_agent_genetic_positive(n_agents=50, n_generations=100, save=True):
-    """
-    This function uses crossover_function_1_1 where a linear interpolation between 0 and 1 is performed to have all
-    scores (positive and negative) scaled and positive between 0 and 1. To give more importance to better scores, the
-    3 power of the scores is taken before normalization.
-    """
-    # initialize environment
-    env = ENVIRONMENT.env
-
-    n_weights = len(Agent(env).model.get_weights())
-
-    agents_weights = np.empty((n_generations, n_agents, n_weights), dtype=np.ndarray)
-    scores = np.empty((n_generations, n_agents), dtype=float)
-    noises = np.empty(n_generations, dtype=float)
-    children = np.empty((n_generations, n_weights), dtype=np.ndarray)
-
-    # initialize agents
-    agents = [Agent(env) for _ in range(n_agents)]
-
-    for i in range(n_generations):
-        agents_weights[i] = np.array([a.model.get_weights() for a in agents], dtype=np.ndarray)
-        print(i)
-
-        for j, agent in enumerate(agents):  # TODO parallelize
-            scores[i][j] = agent.run_agent()
-
-        child = crossover_function_1_1(agents, scores[i])
-        children[i] = np.array(child, dtype=np.ndarray).reshape(n_weights)
-        agents = generate_population(child, n_agents, agents, noise(scores[i]))
-        noises[i] = noise(scores[i])
-        '''
-        child1,child2 = crossover_function_2(agents, scores[i])
-        agents = generate_population_2(child1, child2, n_agents, agents, noise(scores[i]))
-        noises[i] = noise(scores[i])
-        '''
-    if save:
-        save_results(agents_weights, scores)
-    np.savetxt("noise.csv", noises, delimiter=",")
 
     return agents_weights, scores, children
--- src/GA/genetic_functions.py
+++ src/GA/genetic_functions.py
@@ -8,19 +8,6 @@ if RANDOM_SEED:
     np.random.seed(RANDOM_SEED)
 
 
-def noise(rewards):
-    '''
-    This is a test to have a scalable sigmoidal noise to have better exporation depending on the maximum achived score.
-    Not sure if it is better to use the max or the average reward.
-    The noise function is create ad hoc for the problem.
-    :param rewards:
-    :return: noise
-    '''
-    noise = 20 / (1 + exp(0.07 * (rewards.max()))) + 0.1
-    print('max_rew:', rewards.max(), 'avarage_rew:', np.average(rewards), '  noise:', noise)
-    return noise
-
-
 def crossover_function(agents, rewards):
     '''
     :param agents: a list of keras neural networks (parents agents)
@@ -37,85 +24,6 @@ def crossover_function(agents, rewards):
         new_layer = np.zeros_like(agents[0].model.get_weights()[i])
         for j, parent_agent in enumerate(agents):
             layer = parent_agent.model.get_weights()[i] * normalized_rewards[j]
-            new_layer = new_layer + layer
-        child_model.append(new_layer)
-    return child_model
-
-
-def crossover_function_1(agents, reward):
-    '''
-    This one use only the model with a positive score to generate the next population
-    models: a list of keras neural networks (parents agents)
-    rewars: list (or array) of rewards associated to the performance of the
-            corresponding model
-    return: the child weights computed by the weighted average
-            of the parents w.r.t. the reward
-    '''
-    index = 0
-    noise = 0.1
-    rewards = reward.tolist()
-    agents_copy = agents.copy()
-    while index < len(rewards):
-        if rewards[index] < 0:
-            del agents_copy[index]
-            del rewards[index]
-        else:
-            index += 1
-
-    rewards = np.array(rewards)
-    if len(agents_copy) == 0:
-        noise = 0.5
-        # pick the meno peggio, return this as child model and increase the noise in the next gen
-        return agents[np.argmax(reward)].model.get_weights()
-        # sys.exit('Init Fail')
-
-    num_layers = len(agents[0].model.get_weights())
-    normalized_rewards = rewards / np.sum(rewards)
-    child_model = []
-    for i in range(num_layers):
-        new_layer = np.zeros_like(agents_copy[0].model.get_weights()[i])
-        for j, parent_agent in enumerate(agents_copy):
-            layer = parent_agent.model.get_weights()[i] * normalized_rewards[j]
-            new_layer = new_layer + layer
-        child_model.append(new_layer)
-    return child_model
-
-
-def crossover_function_1_1(agents, rewards):
-    '''
-    agents: a list of keras neural networks (parents agents)
-    rewards: list (or array) of rewards associated to the performance of the
-          corresponding model
-    return: the child weights computed by the weighted average
-          of the parents w.r.t. the reward
-    '''
-    rewards = np.array(rewards)
-    scaled_rewards = np.power(np.interp(rewards, (rewards.min(), rewards.max()), (0, 1)), 3)
-    # scaled_rewards = np.exp(rewards)
-    normalized_rewards = scaled_rewards / np.sum(scaled_rewards)
-    num_layers = len(agents[0].model.get_weights())
-    child_model = []
-    for i in range(num_layers):
-        new_layer = np.zeros_like(agents[0].model.get_weights()[i])
-        for j, parent_agent in enumerate(agents):
-            layer = parent_agent.model.get_weights()[i] * normalized_rewards[j]
-            new_layer = new_layer + layer
-        child_model.append(new_layer)
-    return child_model
-
-
-def crossover_function_1_2(agents, rewards):
-    rewards = np.array(rewards)
-    scaled_rewards = np.interp(rewards, (rewards.min(), rewards.max()), (0, 1))
-    ordered_indexes = np.argsort(-scaled_rewards)
-    best_agents = [agents[i] for i in ordered_indexes[:5]]
-    best_scaled_norm_rew = scaled_rewards[ordered_indexes[:5]] / np.sum(scaled_rewards[ordered_indexes[:5]])
-    num_layers = len(agents[0].model.get_weights())
-    child_model = []
-    for i in range(num_layers):
-        new_layer = np.zeros_like(agents[0].model.get_weights()[i])
-        for j, parent_agent in enumerate(best_agents):
-            layer = parent_agent.model.get_weights()[i] * best_scaled_norm_rew[j]
             new_layer = new_layer + layer
         child_model.append(new_layer)
     return child_model
